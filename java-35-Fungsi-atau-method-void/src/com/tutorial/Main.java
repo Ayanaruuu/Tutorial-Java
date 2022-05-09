38f95342e04ebd1de1f892cadedc58e63c823ce5
Ayanaruuu <perdanaarya712@gmail.com>
2022-05-09 19:21:34 +0700

    Java 37 - Fungsi Rekursif (Recursive)

--- java-35-Fungsi-atau-method-void/src/com/tutorial/Main.java
+++ java-35-Fungsi-atau-method-void/src/com/tutorial/Main.java
@@ -2,6 +2,26 @@ package com.tutorial;
 
 public class Main {
     public static void main(String[] args) {
-        
+//        Void itu artinya hampa
+
+        System.out.println(simpel());
+
+        selamatPagi("Ayana");
+        fungsiVoid("Asekk");
+    }
+
+    private static void selamatPagi(String nama) {
+        System.out.println("Selamat Pagi, " + nama);
+    }
+
+//    Fungsi atau method tanpa kembalian
+    private static void fungsiVoid(String input) {
+        System.out.println(input);
+    }
+
+//    Fungsi atau method dengan kembalian
+//    sehingga menggunakan return untuk mengembalikan nilainya
+    private static float simpel() {
+        return 10.0f;
     }
 }
